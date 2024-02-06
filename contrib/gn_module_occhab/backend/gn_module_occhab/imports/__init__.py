98ed6bb95ae8e5f97cdfa9af17cf890d5c4f6883
Élie Bouttier <elie@bouttier.eu>
2024-02-06 21:49:46 +0100

    fixup! Feat(UUID) generate UUID and add check

--- contrib/gn_module_occhab/backend/gn_module_occhab/imports/__init__.py
+++ contrib/gn_module_occhab/backend/gn_module_occhab/imports/__init__.py
@@ -145,10 +145,6 @@ def check_transient_data(task, logger, imprt):
                 generate_altitudes(
                     imprt, fields["the_geom_local"], fields["altitude_min"], fields["altitude_max"]
                 )
-            if imprt.fieldmapping.get("unique_id_sinp_generate", False):
-                generate_missing_uuid(imprt, entity, fields["unique_id_sinp_station"])
-            else:
-                check_missing_uuid(imprt, entity, fields["unique_id_sinp_station"])
 
             check_altitudes(
                 imprt,
@@ -171,9 +167,15 @@ def check_transient_data(task, logger, imprt):
                     fields["geom_local"],
                     id_area=current_app.config["IMPORT"]["ID_AREA_RESTRICTION"],
                 )
+
             if "unique_id_sinp_station" in selected_fields:
                 check_duplicate_uuid(imprt, entity, selected_fields["unique_id_sinp_station"])
                 check_existing_uuid(imprt, entity, selected_fields["unique_id_sinp_station"])
+            if imprt.fieldmapping.get("unique_id_sinp_generate", False):
+                generate_missing_uuid(imprt, entity, fields["unique_id_sinp_station"])
+            else:
+                check_missing_uuid(imprt, entity, fields["unique_id_sinp_station"])
+
             if "id_station_source" in fields:
                 check_duplicate_source_pk(imprt, entity, fields["id_station_source"])
                 # TODO check existing source pk?
@@ -185,7 +187,6 @@ def check_transient_data(task, logger, imprt):
             if "unique_id_sinp_habitat" in selected_fields:
                 check_duplicate_uuid(imprt, entity, selected_fields["unique_id_sinp_habitat"])
                 check_existing_uuid(imprt, entity, selected_fields["unique_id_sinp_habitat"])
-
             if imprt.fieldmapping.get("unique_id_sinp_generate", False):
                 generate_missing_uuid(imprt, entity, fields["unique_id_sinp_habitat"])
             else:
